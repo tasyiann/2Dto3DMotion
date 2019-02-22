fb5188eeb3f2e3bbde4362b010ebc1678285cb6d
Tassos <ay222cn@student.lnu.se>
2019-02-22 13:52:12 +0200

    Feet sliding update

--- Assets/Scripts/NoMoreFeetSliding.cs
+++ Assets/Scripts/NoMoreFeetSliding.cs
@@ -13,21 +13,37 @@ namespace RootMotion.FinalIK
         public Material SlightTransparentMat;
         public Material InvisibleMat;
         public bool showPrediction = false;
+        public bool DebugMode = false;
         //public Renderer[] skins;
         [Range(0.0f,1f)]
         public float secondsPrediction = 0.3f;
+        /// <summary>
+        /// Minimum distance from the feet joint to the ground.
+        /// </summary>
         [Range(0.0f, 1f)]
         public float floorOffset = 0.1f;
+        /// <summary>
+        /// Maximum distance between future and current joint.
+        /// </summary>
+        [Range(0.0f, 1f)]
+        public float maxDistanceBetweenJoints = 0.1f;
+        [Range(0.0f, 1f)]
+        public float feetHeight = 0.3f;
         [Range(0.0f, 1f)]
         public float positionWeight = 1f;
         [Range(0.0f, 1f)]
         public float rotationWeight = 0.5f;
+
         #endregion Parameters
 
-        private FullBodyBipedIK fullBodyBipedIK;
+        private FullBodyBipedIK fullBodyBipedIK_Current;
+        private FullBodyBipedIK fullBodyBipedIK_Future;
 
-        Transform leftFoot;
-        Transform rightFoot;
+        Transform leftFoot_Future;
+        Transform rightFoot_Future;
+        Transform leftFoot_Current;
+        Transform rightFoot_Current;
+
         IKEffector leftFoot_Effector;
         IKEffector rightFoot_Effector;
         bool isLeftFootFrozen = false;
@@ -57,8 +73,11 @@ namespace RootMotion.FinalIK
             currentModel.name = "NoMoreSliding_Humanoid";                // Rename new gameobject.
             currentModel.transform.SetParent(model.transform.parent);    // Place in hierarchy.
             currentModel.transform.position = model.transform.position;
-            fullBodyBipedIK = currentModel.GetComponent<FullBodyBipedIK>();
             currModelRenderers = getSkinsRenderers(currentModel);
+
+            fullBodyBipedIK_Current = currentModel.GetComponent<FullBodyBipedIK>();
+            leftFoot_Effector = fullBodyBipedIK_Current.solver.leftFootEffector;
+            rightFoot_Effector = fullBodyBipedIK_Current.solver.rightFootEffector;
 
 
             /* Future prediction model (model) */
@@ -71,10 +90,11 @@ namespace RootMotion.FinalIK
             Debug.Log("Animation: '" + modelFutureAnimationClip.name + "' will be forwarded by some frames");
             float animationDuration = modelFutureAnimationClip.length;                                            // (1 / this) equals the percentage of a second
             modelFutureAnimator.Play(modelFutureAnimationClip.name, 0, (1/animationDuration)* secondsPrediction); // Make future animation ahead.
-            leftFoot = fullBodyBipedIK.references.leftFoot.transform;     // Get Left foot of futureModel
-            rightFoot = fullBodyBipedIK.references.rightFoot.transform;   // Get Right foot of futureModel
-            leftFoot_Effector = fullBodyBipedIK.solver.leftFootEffector; 
-            rightFoot_Effector = fullBodyBipedIK.solver.rightFootEffector;
+
+            fullBodyBipedIK_Future = futureModel.GetComponent<FullBodyBipedIK>();
+            leftFoot_Future = fullBodyBipedIK_Future.references.leftFoot.transform;     // Get Left foot of futureModel
+            rightFoot_Future = fullBodyBipedIK_Future.references.rightFoot.transform;   // Get Right foot of futureModel
+
 
 
             // Targets
@@ -120,8 +140,8 @@ namespace RootMotion.FinalIK
 
         private void Update()
         {
-            examineAndFixFoot(ref isLeftFootFrozen, leftFoot, leftFoot_Effector, ref leftFootTarget, "LeftFootTarget");
-            examineAndFixFoot(ref isRightFootFrozen, rightFoot, rightFoot_Effector, ref rightFootTarget, "RightFootTarget");
+            examineAndFixFoot(ref isLeftFootFrozen, leftFoot_Future, leftFoot_Current, leftFoot_Effector, ref leftFootTarget, "LeftFootTarget");
+            examineAndFixFoot(ref isRightFootFrozen, rightFoot_Future, rightFoot_Current, rightFoot_Effector, ref rightFootTarget, "RightFootTarget");
             predictionModelAppear();
         }
 
@@ -141,31 +161,48 @@ namespace RootMotion.FinalIK
         }
 
 
-        private void examineAndFixFoot(ref bool isFootFrozen, Transform foot, IKEffector footEffector, ref GameObject footTarget, string name)
+        private void examineAndFixFoot(ref bool isFootFrozen, Transform foot_future, Transform foot_current, IKEffector footEffector, ref GameObject footTarget, string name)
         {
             bool intersectionFound;
-            RaycastHit intersection = groundIntersection(foot.transform, out intersectionFound);
-            float distance = intersection.distance;
-            if (intersectionFound && !isFootFrozen  && distance < floorOffset)
+            RaycastHit intersection = groundIntersection(foot_future.transform, out intersectionFound);
+            float distanceFromGround = intersection.distance;
+            if (intersectionFound && !isFootFrozen  && distanceFromGround < (floorOffset-feetHeight))
             {
-                updateTargetPosition(footTarget, intersection.point);
+                // Place feet above the ground plane, not in the ground plane.
+                Vector3 targetPosition = new Vector3(intersection.point.x, intersection.point.y + feetHeight, intersection.point.z);
+                updateTargetPosition(footTarget, targetPosition);
                 stickOnGround(footEffector);
                 isFootFrozen = true;
-                setSkinsColor(Color.red, currentModel);
-                Debug.Log(foot.name + " is STUCK.");
+                if (DebugMode)
+                {
+                    setSkinsColor(Color.red, currentModel);
+                    Debug.Log(foot_future.name + " - preventing Foot Sliding");
+                }
+
             }
-            if (intersectionFound && isFootFrozen && distance >= floorOffset)
+            if (intersectionFound && isFootFrozen && distanceFromGround >= (floorOffset-feetHeight))
             {
                 releaseFromGround(footEffector);
                 isFootFrozen = false;
-                setSkinsColor(Color.blue, currentModel);
-                //Debug.Log(foot.name + " is RELEASED.");
+                if (DebugMode)
+                {
+                    setSkinsColor(Color.blue, currentModel);
+                    //Debug.Log(foot.name + " is RELEASED.");
+                }
+
             }
         }
+
+        private float getDistanceBetweenJoints(Transform foot_future, Transform foot_current)
+        {
+            return Vector3.Distance(foot_future.position, foot_current.position);
+        }
+
 
         private void updateTargetPosition(GameObject footTarget, Vector3 position)
         {
             footTarget.transform.position = position;
+            
         }
 
         private void stickOnGround(IKEffector effector)
